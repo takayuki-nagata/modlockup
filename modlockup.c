d4bbbc98eed99193964c6f8c98018ccc1e39f8e8
Takayuki Nagata <tnagata@redhat.com>
2018-08-17 12:54:45 +0900

    Add duration featuire

--- modlockup.c
+++ modlockup.c
@@ -9,11 +9,15 @@
 
 static unsigned int cpu = 0;
 module_param(cpu, uint, 0);
-MODULE_PARM_DESC(cpu, "Set a target CPU");
+MODULE_PARM_DESC(cpu, "Set a target CPU (default: 0)");
 
 static unsigned long delay = 300;
 module_param(delay, ulong, 0);
-MODULE_PARM_DESC(delay, "Set delay for lockup in seconds");
+MODULE_PARM_DESC(delay, "Set delay for lockup in seconds (default: 300)");
+
+static unsigned long duration = 10;
+module_param(duration, ulong, 0);
+MODULE_PARM_DESC(duration, "Set locup duration in seconds (default: 10)");
 
 MODULE_AUTHOR("Takayuki Nagata <tnagata@redhat.com>");
 MODULE_LICENSE("GPL");
@@ -22,43 +26,57 @@ struct task_struct *task;
 
 static void doit(void)
 {
-	printk(KERN_EMERG "lockup: !!CPU%d IS LOCKED!!\n", cpu);
-	while (1) { 
+	unsigned long start = local_clock() >> 30LL;
+
+	printk(KERN_EMERG "lockup: !!CPU%d WILL BE LOCKING FOR %ld SECONDS!!\n", cpu, duration);
+	while (1) {
+		unsigned long now;
+		now = local_clock() >> 30LL;
+		if (time_after(now, start + duration)) {
+			break;
+		}
 	}
+	printk("lockup: CPU%d will be released\n", cpu);
+
+	return;
 }
 
 static int lockup_thread(void *data)
 {
 	unsigned long start = local_clock() >> 30LL;
 
-	printk(KERN_EMERG "lockup: CPU%d will hang after %ld seconds. Unload the module to stop lockup.\n", cpu, delay);
-	while (1) {
+	printk(KERN_EMERG "lockup: CPU%d will be locked for %ld seconds after %ld seconds."
+		 "Unload the module to stop lockup.\n", cpu, duration, delay);
+	while (!kthread_should_stop()) {
 		unsigned long now;
-		if (kthread_should_stop()) {
-			printk("lockup: stopping lockup thread.\n");
-			return 0;
-		}
+
 		now = local_clock() >> 30LL;
 		if (time_after(now, start + delay)) {
 			doit();
+			break;
+		} else {
+			cond_resched();
 		}
-		schedule();
 	}
+	printk("lockup: stopping lockup thread.\n");
+
+	task = NULL;
 	return 0;
 }
 
 static int __init modlockup_init(void)
 {
-	task = kthread_create(lockup_thread, NULL, "lockup/%d", cpu);
+	int ret = 0;
 
+	task = kthread_create(lockup_thread, NULL, "lockup/%d", cpu);
 	if (task) {
 		kthread_bind(task, cpu);
 		wake_up_process(task);
 	} else {
-		return -ENOMEM;
+		ret = -ENOMEM;
 	}
 
-	return 0;
+	return ret;
 }
 
 static void __exit modlockup_exit(void)
